c796ea2cfb36990d046e4df41136b230318c34b7
Priyanka Gawali <149878203+priyankagawali09@users.noreply.github.com>
2026-08-04 15:49:51 +0530

    Time: 7 ms (69.37%), Space: 44.2 MB (37.33%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+         if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(count1, count2)) {
+            return true;
+        }
+
+        int left = 0;
+
+        for (int right = s1.length(); right < s2.length(); right++) {
+
+            count2[s2.charAt(right) - 'a']++;
+
+            count2[s2.charAt(left) - 'a']--;
+
+            left++;
+
+            if (matches(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    
+    }
+}
